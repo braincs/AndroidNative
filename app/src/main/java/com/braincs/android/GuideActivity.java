49db068126a7f6b93a70455e4561f85f3700128b
shuai <8019969+braincs@users.noreply.github.com>
2021-04-12 17:32:05 +0800

    update: GuideActivity

--- app/src/main/java/com/braincs/android/GuideActivity.java
+++ app/src/main/java/com/braincs/android/GuideActivity.java
@@ -1,23 +1,38 @@
 package com.braincs.android;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
+import com.braincs.nativebytes.CameraPreviewActivity;
 import com.braincs.nativedlopen.NDlopenActivity;
 
 public class GuideActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, 0);
+        }
     }
 
     public void demoDlopenActivity(View view) {
         Intent intent = new Intent(GuideActivity.this, NDlopenActivity.class);
+        startActivity(intent);
+    }
+
+    public void demoNativeBytesActivity(View view) {
+        Intent intent = new Intent(GuideActivity.this, CameraPreviewActivity.class);
         startActivity(intent);
     }
 }
